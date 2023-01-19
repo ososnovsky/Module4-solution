d8a1eb81ddc909278e900c70612fe8dc710590a7
ososnovsky <oleg.sosnovsky@gmail.com>
2023-01-19 21:55:39 +0200

    repeat commit

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
+
 for (var i = 0; i < names.length; i++) {
 
   // STEP 11:
@@ -51,6 +52,7 @@ for (var i = 0; i < names.length; i++) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
+
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12:
@@ -58,6 +60,7 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
+  
   if (firstLetter === "j") {
   byeSpeaker.speak (names [i]);
   } else {
